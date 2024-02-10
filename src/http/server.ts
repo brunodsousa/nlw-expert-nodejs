a7ade20e603c96f8799b70ab752433a568629116
Bruno Sousa <brunodsousa@outlook.com>
2024-02-10 07:07:38 -0300

    create poll route
    adds options when creating poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,28 +1,9 @@
 import fastify, { FastifyReply, FastifyRequest } from "fastify";
-import { PrismaClient } from "@prisma/client";
-import { z } from "zod";
+import { createPoll } from "./routes/create-poll";
 
 const app = fastify();
 
-const prisma = new PrismaClient();
-
-app.post("/polls", async (request: FastifyRequest, reply: FastifyReply) => {
-  const createPollBody = z.object({
-    title: z.string(),
-  });
-
-  const { title } = createPollBody.parse(request.body);
-
-  const poll = await prisma.poll.create({
-    data: {
-      title,
-    },
-  });
-
-  return reply.status(201).send({
-    pollId: poll.id,
-  });
-});
+app.register(createPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
