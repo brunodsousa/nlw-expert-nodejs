bb47fcb991f407754174d7e9198dee6d23852c73
Bruno Sousa <brunodsousa@outlook.com>
2024-02-11 19:09:32 -0300

    vote on poll route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,20 @@
-import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: process.env["FASTIFY_COOKIE_SECRET"],
+  hook: "onRequest",
+  parseOptions: {},
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
