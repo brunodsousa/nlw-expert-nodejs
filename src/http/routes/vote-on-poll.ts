ec3e9b8d0b33f3b2c1aeaf55d8bf57ea29613e40
Bruno Sousa <brunodsousa@outlook.com>
2024-02-12 12:30:59 -0300

    votes ranking system

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import z from "zod";
 import { randomUUID } from "node:crypto";
 import { prisma } from "../../lib/prisma";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post(
@@ -39,6 +40,8 @@ export async function voteOnPoll(app: FastifyInstance) {
               id: userPreviousVoteOnPoll.id,
             },
           });
+
+          await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
         } else if (userPreviousVoteOnPoll) {
           return reply
             .status(400)
@@ -64,6 +67,8 @@ export async function voteOnPoll(app: FastifyInstance) {
           pollOptionId,
         },
       });
+
+      await redis.zincrby(pollId, 1, pollOptionId);
 
       return reply.status(201);
     }
