1f1cf1aab28a17447499a3614024deeb5b139c49
Bruno Sousa <brunodsousa@outlook.com>
2024-02-12 14:27:33 -0300

    realtime voting system

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -3,6 +3,7 @@ import z from "zod";
 import { randomUUID } from "node:crypto";
 import { prisma } from "../../lib/prisma";
 import { redis } from "../../lib/redis";
+import { voting } from "../../utils/voting-pub-sub";
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post(
@@ -41,7 +42,16 @@ export async function voteOnPoll(app: FastifyInstance) {
             },
           });
 
-          await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId);
+          const votes = await redis.zincrby(
+            pollId,
+            -1,
+            userPreviousVoteOnPoll.pollOptionId
+          );
+
+          voting.publish(pollId, {
+            pollOptionId: userPreviousVoteOnPoll.pollOptionId,
+            votes: Number(votes),
+          });
         } else if (userPreviousVoteOnPoll) {
           return reply
             .status(400)
@@ -68,7 +78,12 @@ export async function voteOnPoll(app: FastifyInstance) {
         },
       });
 
-      await redis.zincrby(pollId, 1, pollOptionId);
+      const votes = await redis.zincrby(pollId, 1, pollOptionId);
+
+      voting.publish(pollId, {
+        pollOptionId,
+        votes: Number(votes),
+      });
 
       return reply.status(201);
     }
